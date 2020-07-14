d6604f6fa529f440c770aaa6f0bbaff2a99ae77e
Hannah Livnat <hannahlivnat@gmail.com>
2020-07-13 21:50:19 -0700

    show route for find by username works

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,4 +2,7 @@ class UserController < ApplicationController
   def index
     render json: User.all
   end
+  def show
+    render json: User.find_by(user_name: params["username"])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   get '/', to: 'component#index'
   get'/component/:id', to: 'component#show'
-  post '/component/', to: 'component#create'
+  post '/component/', to: 
+'component#create'
   put '/component/:id', to: 'component#update'
   delete '/component/:id', to: 'component#destroy'
 
   get '/user', to: 'user#index'
+  get '/user/:username', to: 'user#show'
 end
