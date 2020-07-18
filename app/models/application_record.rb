9db5d182a1fe9c17a532dcfe10844368bfbba7e3
Hannah Livnat <hannahlivnat@gmail.com>
2020-07-18 11:09:53 -0700

    deleted active base connection in application controller

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,10 +1,3 @@
-ActiveRecord::Base.establish_connection(
-  adapter: "postgresql", 
-  host: "localhost", 
-  port: 5432, 
-  dbname: 'ComponentLibrary_development'
-)
-
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 end
