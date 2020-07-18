7fdd931786f1578212852cf78590bd5718918f6a
Hannah Livnat <hannahlivnat@gmail.com>
2020-07-18 11:55:09 -0700

    deleted title from serializer for component file

--- app/serializers/component_serializer.rb
+++ app/serializers/component_serializer.rb
@@ -1,3 +1,3 @@
 class ComponentSerializer < ActiveModel::Serializer
-  attributes :id, :image, :title, :description, :code_block, :tags, :public, :user_id
+  attributes :id, :image, :description, :code_block, :tags, :public, :user_id
 end
